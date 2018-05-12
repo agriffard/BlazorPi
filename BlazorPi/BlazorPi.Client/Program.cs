9882fddb29bde8ee13edd904b3ab3517d7d50a4e
Antoine Griffard <agriffard@hotmail.com>
2018-05-12 17:24:07 +0200

    First commit

---
+++ BlazorPi/BlazorPi.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using BlazorPi.Client.Services;
+using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BlazorPi.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(services =>
+            {
+                services.AddSingleton<PiState>();
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BlazorPi
-Blazor Pi
+Blazor Pi allows you to test how much decimals of Pi you know.
+
+## Tools
+
+The pre-requisites are:
+
+* [.NET Core 2.1 Preview 2 SDK](https://www.microsoft.com/net/download/dotnet-core/sdk-2.1.300-preview2)
+* [Visual Studio 2017 (15.7)](https://www.visualstudio.com/vs/preview/)
+* ASP.NET and web development workload for Visual Studio
+* [ASP.NET Core Blazor Language Services extension](https://go.microsoft.com/fwlink/?linkid=870389)
